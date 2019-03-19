9f8c295b193b84338fa76ac12cadfa4041aa2dd0
Felipe Batista <felipeb@benetech.org@13401-labs.anmqudxzzydufor3sw5p2c0w1f.xx.internal.cloudapp.net>
2019-03-19 02:33:35 +0000

    Script API and useful bash commands

---
+++ extract_features.py
@@ -0,0 +1,76 @@
+import numpy as np
+import os
+from glob import glob
+from winnow.feature_extraction import IntermediateCnnExtractor,frameToVideoRepresentation,SimilarityModel
+
+
+with open("config.yaml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+
+# SOURCE Directory
+
+DATASET_DIR = cfg['video_source_folder']
+DST_DIR = cfg['destination_folder']
+VIDEO_LIST_TXT = cfg['video_list_filename']
+ROOT_FOLDER_INTERMEDIATE_REPRESENTATION = CFG['root_folder_intermediate']
+
+print('Searching for Dataset Video Files')
+
+videos = glob(DATASET_DIR + '**')
+
+print('Number of files found: {}'.format(len(videos)))
+
+with open(VIDEO_LIST_TXT, 'w') as f:
+    for item in videos:
+        f.write("%s\n" % item)
+        
+VIDEOS_LIST = os.path.abspath(VIDEO_LIST_TXT)
+
+print('Video List saved on :{}'.format(VIDEOS_LIST))
+
+representations = ['frame_level','video_level','video_signatures']
+
+
+FRAME_LEVEL_SAVE_FOLDER = os.path.abspath(DST_DIR + '{}/{}'.format(ROOT_FOLDER_INTERMEDIATE_REPRESENTATION,representations[0]))
+VIDEO_LEVEL_SAVE_FOLDER = DST_DIR + '{}/{}'.format(ROOT_FOLDER_INTERMEDIATE_REPRESENTATION,representations[1])
+VIDEO_SIGNATURES_SAVE_FOLDER = DST_DIR + '{}/{}'.format(ROOT_FOLDER_INTERMEDIATE_REPRESENTATION,representations[2])
+VIDEO_SIGNATURES_FILENAME = 'video_signatures.npy'
+
+print('Creating Intermediate Representations folder on :{}'.format(DST_DIR))
+
+for r in representations:
+    try:
+        os.makedirs(DST_DIR + '{}/{}'.format(ROOT_FOLDER_INTERMEDIATE_REPRESENTATION,r))
+    except Exception as e:
+        print(e)
+
+    
+# Instantiates the extractor
+extractor = IntermediateCnnExtractor(VIDEOS_LIST,FRAME_LEVEL_SAVE_FOLDER)
+# Starts Extracting Frame Level Features
+extractor.start(batch_size=64,cores=6)
+
+
+
+print('Converting Frame by Frame representatinos to Video Representations')
+
+converter = frameToVideoRepresentation(FRAME_LEVEL_SAVE_FOLDER,VIDEO_LEVEL_SAVE_FOLDER)
+
+converter.start()
+
+
+print('Extracting Signatures from Video representations')
+
+
+sm = SimilarityModel()
+video_signatures = sm.predict(VIDEO_LEVEL_SAVE_FOLDER)
+
+video_signatures = np.nan_to_num(video_signatures)
+
+print('Saving Video Signatures on :{}'.format(VIDEO_SIGNATURES_SAVE_FOLDER))
+
+np.save(os.path.join(VIDEO_SIGNATURES_SAVE_FOLDER,VIDEO_SIGNATURES_FILENAME))
+
+print('Signatures of shape {} saved on :{}'.format(video_signatures.shape,VIDEO_SIGNATURES_SAVE_FOLDER))
+
---
+++ generate_matches.py
@@ -0,0 +1,77 @@
+from glob import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+from winnow.feature_extraction import SimilarityModel
+
+import yaml
+
+print('Loading config file')
+
+with open("../config.yaml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+
+VIDEO_SIGNATURES_SAVE_FOLDER = cfg['video_signatures_folder'] 
+DISTANCE = float(cfg['match_distance'])
+DST_FOLDER = cfg['destination_folder']
+VIDEO_LEVEL_SAVE_FOLDER = cfg['video_level_folder']
+
+print('Extracting Video Signatures')
+sm = SimilarityModel()
+video_signatures = sm.predict(VIDEO_LEVEL_SAVE_FOLDER)
+video_signatures = np.nan_to_num(video_signatures)
+labels = np.array([x.split('_vgg')[0].split('/')[-1] for x in  sm.index])
+
+
+def filter_results(thr):
+    results = []
+    results_distances = []
+    msk = distances < thr
+    for i,r in enumerate(msk):
+        results.append(indices[i,r])
+        results_distances.append(distances[i,r])
+    return results,results_distances
+
+
+
+
+print('Finding Matches...')
+nn = NearestNeighbors(n_neighbors=50,metric='euclidean',algorithm='kd_tree')
+nn.fit(video_signatures)
+distances,indices = nn.kneighbors(video_signatures)
+
+
+results,results_distances = filter_results(DISTANCE)
+
+ss = sorted(zip(results,results_distances),key=lambda x:len(x[0]),reverse=True)
+results_sorted = [x[0] for x in ss]
+results_sorted_distance = [x[1] for x in ss]
+
+
+q = []
+m = []
+distance = []
+
+print('Generating Report')
+for i,r in enumerate(results_sorted):
+    for j,matches in enumerate(r):
+        if j == 0:
+            qq = matches
+        q.append(qq)
+        m.append(matches)
+        distance.append(results_sorted_distance[i][j])
+
+match_df = pd.DataFrame({"query":q,"match":m,"distance":distance})            
+match_df['query_video'] = labels[match_df['query']]
+match_df['match_video'] = labels[match_df['match']]
+
+
+
+REPORT_PATH = DST_FOLDER + '/matches_at_{}_distance.csv'.format(DISTANCE)
+
+print('Saving report to {}'.format(REPORT_PATH))
+
+match_df.to_csv(REPORT_PATH)
