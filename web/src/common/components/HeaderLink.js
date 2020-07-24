82ec54339690a6b6276dac850dd8cfe2057bfd3d
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-07-24 15:46:21 +0700

    Make nav-link width constant.
    When navigation link is selected its font-weight is changed to bold
    and hance element width changes making entire link list to jump
    whenever a new element is selected. This change ensure all links
    have the same size whether they are selected or not.

--- web/src/common/components/HeaderLink.js
+++ web/src/common/components/HeaderLink.js
@@ -2,18 +2,61 @@ import React from "react";
 import clsx from "clsx";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/styles";
-import Label from "./Label";
 import SelectionDecorator from "./SelectionDecorator";
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    flexGrow: 1,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   link: {
-    padding: 4,
-    paddingBottom: 10,
+    padding: "5px 4px 8px 4px",
     cursor: "pointer",
+    /**
+     * Ensure selection decorator is displayed correctly.
+     */
     transform: "translate(0%, 0px)",
+  },
+  textSelected: {
+    fontWeight: "bold",
+    color: theme.palette.primary.main,
+  },
+  textUnselected: {
+    fontWeight: "normal",
+    color: theme.palette.common.black,
+  },
+  /**
+   * Wrap link text in additional component to ensure
+   * it's total width doesn't change when font weight
+   * changes.
+   */
+  text: {
+    ...theme.mixins.navlink,
+    display: "inline-block",
+    textAlign: "center",
+    /**
+     * Reserve space for bolded content in ::before
+     * pseudo element.
+     */
+    "&::before": {
+      ...theme.mixins.navlink,
+      fontWeight: "bold",
+      display: "block",
+      content: "attr(title)",
+      height: 0,
+      overflow: "hidden",
+      visibility: "hidden",
+    },
   },
 }));
 
+/**
+ * Single navigation link.
+ *
+ * @see HeaderLinks
+ */
 function HeaderLink(props) {
   const { title, selected, onClick, className } = props;
   const classes = useStyles();
@@ -21,15 +64,19 @@ function HeaderLink(props) {
   const decorator = selected ? <SelectionDecorator variant="bottom" /> : null;
 
   return (
-    <div className={clsx(classes.link, className)} onClick={onClick}>
-      <Label
-        variant="navlink"
-        bold={selected}
-        color={selected ? "primary" : "black"}
-      >
-        {title}
-      </Label>
-      {decorator}
+    <div className={clsx(classes.container, className)} onClick={onClick}>
+      <div className={classes.link}>
+        <div
+          title={title}
+          className={clsx(classes.text, {
+            [classes.textSelected]: selected,
+            [classes.textUnselected]: !selected,
+          })}
+        >
+          {title}
+        </div>
+        {decorator}
+      </div>
     </div>
   );
 }
