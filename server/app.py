dd6783adfda34e18ff52eda70f781a4ffd4847d8
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-08-04 00:10:14 +0700

    Serve static files

--- server/app.py
+++ server/app.py
@@ -1,21 +1,26 @@
+import yaml
+from api import api as api_blueprint
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import yaml
 
 with open("config.yaml", 'r') as config:
     cfg = yaml.load(config)
 
 CONNINFO = cfg['conninfo']
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
 db = SQLAlchemy(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = CONNINFO
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from api import api as api_blueprint
-
 app.register_blueprint(api_blueprint, url_prefix='/api/v1')
+
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
