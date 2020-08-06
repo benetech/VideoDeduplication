3f4a52f4345c41d64e231a0e19ca9edcc3367e6a
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-08-06 15:35:07 +0700

    Use environment variables to configure server

--- server/app.py
+++ server/app.py
@@ -1,26 +1,55 @@
-import yaml
-from api import api as api_blueprint
+import fire
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-with open("config.yaml", 'r') as config:
-    cfg = yaml.load(config)
-
-CONNINFO = cfg['conninfo']
-
-app = Flask(__name__, static_url_path="")
-db = SQLAlchemy(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = CONNINFO
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-app.register_blueprint(api_blueprint, url_prefix='/api/v1')
+from api import api as api_blueprint
+from config import Config
+from model import database
 
 
-@app.route('/')
-def root():
-    return app.send_static_file('index.html')
+def connection(config):
+    """Get database connection URI."""
+    db = config.database
+    return f"postgres://{db.user}:{db.password}@{db.host}:{db.port}/{db.name}"
+
+
+def create_application(config):
+    """Create configured flask application."""
+    app = Flask(__name__, static_url_path="", static_folder=config.static_folder)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = connection(config)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
+
+    @app.route('/')
+    def root():
+        return app.send_static_file('index.html')
+
+    return app
+
+
+def serve(host=None, port=None, db_host=None, db_port=None, db_name=None, db_user=None, db_secret=None):
+    """Start Deduplication API Server."""
+
+    # Read configuration
+    config = Config()
+    config.port = port or config.port
+    config.host = host or config.host
+    config.database.port = db_port or config.database.port
+    config.database.host = db_host or config.database.host
+    config.database.name = db_name or config.database.name
+    config.database.user = db_user or config.database.user
+    config.database.secret = db_secret or config.database.secret
+
+    # Create application
+    application = create_application(config)
+
+    # Initialize database
+    database.init_app(application)
+
+    # Serve REST API
+    application.run(host=config.host, port=config.port)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    fire.Fire(serve)
---
+++ server/config.py
@@ -0,0 +1,27 @@
+import os
+
+
+class DatabaseConfig:
+    """Database connection configuration."""
+    port = os.environ.get("DATABASE_PORT", 5432)
+    host = os.environ.get("DATABASE_HOST", "localhost")
+    name = os.environ.get("DATABASE_NAME", "videodeduplicationdb")
+    user = os.environ.get("DATABASE_USER", "postgres")
+    env_password = os.environ.get("DATABASE_PASS", "admin")
+    secret = os.environ.get("DATABASE_SECRET")
+
+    @property
+    def password(self):
+        """Get database password"""
+        if self.secret is not None:
+            with open(self.secret, 'r') as secret:
+                return secret.read()
+        return self.env_password
+
+
+class Config:
+    """Server configuration."""
+    database = DatabaseConfig()
+    host = os.environ.get("SERVER_HOST", "0.0.0.0")
+    port = os.environ.get("SERVER_PORT", 5000)
+    static_folder = os.environ.get("STATIC_FOLDER", "static")
