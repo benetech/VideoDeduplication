27edb68fb7e8f4fee44b6f612013747ae50e6fa2
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-08-18 18:41:50 +0700

    Outline video-page layout

--- web/src/collection/components/VideoDetails/VideoDetails.js
+++ web/src/collection/components/VideoDetails/VideoDetails.js
@@ -3,14 +3,73 @@ import clsx from "clsx";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/styles";
 import { useParams } from "react-router-dom";
+import { useIntl } from "react-intl";
+import Paper from "@material-ui/core/Paper";
+import Button from "../../../common/components/Button";
+import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "stretch",
+    padding: theme.dimensions.content.padding,
+    paddingTop: theme.dimensions.content.padding * 2,
+  },
+  actions: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: theme.spacing(2),
+  },
+  header: {
+    boxShadow: "0 12px 18px 0 rgba(0,0,0,0.08)",
+    margin: theme.spacing(2),
+    padding: theme.spacing(4),
+  },
+  dataContainer: {
+    padding: theme.spacing(2),
+  },
+  video: {},
+  info: {},
+}));
+
+/**
+ * Get i18n messages
+ */
+function useMessages() {
+  const intl = useIntl();
+  return {
+    compare: intl.formatMessage({ id: "actions.compare" }),
+  };
+}
 
 function VideoDetails(props) {
   const { className } = props;
+  const messages = useMessages();
   const { id } = useParams();
   const classes = useStyles();
-  return <div className={clsx(className)}>Displaying {id}</div>;
+
+  return (
+    <div className={clsx(classes.root, className)}>
+      <div className={classes.actions}>
+        <Button color="primary" variant="contained">
+          {messages.compare}
+        </Button>
+      </div>
+      <Paper className={classes.header}>header</Paper>
+      <div className={classes.dataContainer}>
+        <Grid container spacing={5}>
+          <Grid item xs={12} lg={6}>
+            <Paper className={classes.video}>Video</Paper>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <Paper className={classes.info}>Video Information</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
 VideoDetails.propTypes = {
