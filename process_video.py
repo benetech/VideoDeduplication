fa2b4e06b23c44e9fd90b56d84d7a83a80f8af59
Felipe Batista <fsbatista1@gmail.com>
2020-08-19 15:22:40 -0300

    Added support for saving to the DB on the single video file processing script

--- process_video.py
+++ process_video.py
@@ -1,11 +1,15 @@
 import click
 import yaml
 import os
+import numpy as np
+import yaml
 from winnow.feature_extraction.loading_utils import global_vector_from_tensor
 from winnow.feature_extraction.extraction_routine import load_featurizer
 from winnow.feature_extraction import SimilarityModel
 from winnow.feature_extraction.utils import download_pretrained,load_video
-import numpy as np
+from db import *
+from db.schema import *
+
 
 @click.command()
 @click.argument('path')
@@ -18,13 +22,13 @@ import numpy as np
 @click.option('--save-frames',is_flag=True)
 @click.option('--save-features/--no-features',default=True)
 @click.option('--save-signatures/--no-signatures',default=True)
+@click.option('--save-db',is_flag=True)
 
-
-def main(path,output,config,save_frames,save_features,save_signatures):
+def main(path,output,config,save_frames,save_features,save_signatures,save_db):
     """
     Application to extract features from a single video file
     """
-    print(save_frames,save_features,save_signatures)
+    print(save_db)
     
 
     PRETRAINED_LOCAL_PATH = download_pretrained(config)
@@ -56,6 +60,30 @@ def main(path,output,config,save_frames,save_features,save_signatures):
         signatures_path = os.path.join(output, '{}_{}_signature'.format(video_name, model.net_name))
         np.save(signatures_path, video_signatures)
     
+    if save_db:
+
+        with open(config, 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+
+        CONNINFO = cfg['conninfo']
+        
+        db_engine,session = create_engine_session(CONNINFO)
+        create_tables(db_engine)
+        #TODO Currently we have an automated incremental index set for the Signatures table (we might want to change it in the future so we don't add duplicated signatures)
+        add_signatures(session,video_signatures,sm.original_filenames)
+
+        try:
+            session.commit()
+    
+        except Exception as e:
+            session.rollback()
+            print('DB Exception',e)
+
+        finally:
+            # Get DB stats
+            signatures = get_all(session,Signature)
+            print(f"Signatures table rows:{len(signatures)}")
+
 
     
     
