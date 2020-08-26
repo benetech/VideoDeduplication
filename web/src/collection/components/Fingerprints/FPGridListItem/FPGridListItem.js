054e419cc9fdd3ffa989f6deb43d5a9f41c0ce8f
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-08-26 17:23:48 +0700

    Extract grid item container

--- web/src/collection/components/Fingerprints/FPGridListItem/FPGridListItem.js
+++ web/src/collection/components/Fingerprints/FPGridListItem/FPGridListItem.js
@@ -3,13 +3,10 @@ import clsx from "clsx";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/styles";
 import { FingerprintType } from "../type";
-import Paper from "@material-ui/core/Paper";
 import MediaPreview from "../../../../common/components/MediaPreview";
 import VideocamOutlinedIcon from "@material-ui/icons/VideocamOutlined";
 import MoreHorizOutlinedIcon from "@material-ui/icons/MoreHorizOutlined";
 import IconButton from "@material-ui/core/IconButton";
-import Grid from "@material-ui/core/Grid";
-import { composition } from "../FPGridList";
 import AttributeText from "../../../../common/components/AttributeText";
 import { useIntl } from "react-intl";
 import {
@@ -21,15 +18,9 @@ import EventAvailableOutlinedIcon from "@material-ui/icons/EventAvailableOutline
 import ExifIcon from "../../../../common/components/icons/ExifIcon";
 import VolumeOffOutlinedIcon from "@material-ui/icons/VolumeOffOutlined";
 import Marked from "../../../../common/components/Marked";
+import FpGridListItemContainer from "./FPGridListItemContainer";
 
 const useStyles = makeStyles((theme) => ({
-  itemContainer: {},
-  gridItem: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-around",
-    boxShadow: "0 12px 18px 0 rgba(0,0,0,0.08)",
-  },
   asButton: {
     cursor: "pointer",
   },
@@ -108,95 +99,81 @@ const FpGridListItem = React.memo(function FpGridListItem(props) {
 
   const intl = useIntl();
   const messages = useMessages(intl);
-  const decrease = dense ? 1 : 0;
   const handleClick = useCallback(() => onClick(file), [file, onClick]);
 
   const classes = useStyles();
   return (
-    <Grid
-      item
-      xs={12 / Math.max(composition.xs - decrease, 1)}
-      sm={12 / Math.max(composition.sm - decrease, 1)}
-      md={12 / Math.max(composition.md - decrease, 1)}
-      lg={12 / Math.max(composition.lg - decrease, 1)}
-      xl={12 / Math.max(composition.xl - decrease, 1)}
-      className={classes.itemContainer}
+    <FpGridListItemContainer
+      className={clsx(button && classes.asButton, className)}
+      dense={dense}
+      onClick={handleClick}
     >
-      <Paper
-        className={clsx(
-          classes.gridItem,
-          button && classes.asButton,
-          className
-        )}
-        onClick={handleClick}
-      >
-        <MediaPreview
-          src={file.preview}
-          alt="preview"
-          className={classes.preview}
+      <MediaPreview
+        src={file.preview}
+        alt="preview"
+        className={classes.preview}
+      />
+      <div className={classes.nameContainer}>
+        <div className={classes.iconContainer}>
+          <VideocamOutlinedIcon className={classes.icon} />
+        </div>
+        <div className={classes.name}>
+          <Marked mark={highlight}>{file.filename}</Marked>
+        </div>
+        <IconButton size="small">
+          <MoreHorizOutlinedIcon fontSize="small" />
+        </IconButton>
+      </div>
+      <div className={classes.attrRow}>
+        <AttributeText
+          name={messages.attr.fingerprint}
+          value={file.fingerprint}
+          variant="primary"
+          size="small"
         />
-        <div className={classes.nameContainer}>
-          <div className={classes.iconContainer}>
-            <VideocamOutlinedIcon className={classes.icon} />
-          </div>
-          <div className={classes.name}>
-            <Marked mark={highlight}>{file.filename}</Marked>
-          </div>
-          <IconButton size="small">
-            <MoreHorizOutlinedIcon fontSize="small" />
-          </IconButton>
+        <div className={classes.dividerContainer}>
+          <div className={classes.divider} />
         </div>
-        <div className={classes.attrRow}>
-          <AttributeText
-            name={messages.attr.fingerprint}
-            value={file.fingerprint}
-            variant="primary"
-            size="small"
-          />
-          <div className={classes.dividerContainer}>
-            <div className={classes.divider} />
-          </div>
-          <AttributeText
-            name={messages.attr.quality}
-            value={file.metadata.quality}
-            defaultValue="Unknown"
-            variant="primary"
-            size="small"
-          />
-          <div className={classes.dividerContainer}>
-            <div className={classes.divider} />
-          </div>
-          <AttributeText
-            name={messages.attr.duration}
-            value={formatDuration(file.metadata.length, intl)}
-            variant="primary"
-            size="small"
-          />
+        <AttributeText
+          name={messages.attr.quality}
+          value={file.metadata.quality}
+          defaultValue="Unknown"
+          variant="primary"
+          size="small"
+        />
+        <div className={classes.dividerContainer}>
+          <div className={classes.divider} />
         </div>
-        <div className={classes.attrRow}>
-          <AttributeText
-            value={formatDate(file.metadata.uploadDate, intl)}
-            icon={EventAvailableOutlinedIcon}
-            variant="normal"
-            defaultValue="Unknown"
-            size="small"
-          />
-          <div className={classes.dividerContainer}>
-            <div className={classes.divider} />
-          </div>
-          <AttributeText
-            value={formatBool(file.metadata.hasEXIF, intl)}
-            icon={ExifIcon}
-            variant="primary"
-            size="small"
-          />
-          <div className={classes.dividerContainer}>
-            <div className={classes.divider} />
-          </div>
-          <VolumeOffOutlinedIcon className={classes.volume} />
+        <AttributeText
+          name={messages.attr.duration}
+          value={formatDuration(file.metadata.length, intl)}
+          variant="primary"
+          size="small"
+        />
+      </div>
+      <div className={classes.attrRow}>
+        <AttributeText
+          value={formatDate(file.metadata.uploadDate, intl)}
+          icon={EventAvailableOutlinedIcon}
+          variant="normal"
+          defaultValue="Unknown"
+          size="small"
+        />
+        <div className={classes.dividerContainer}>
+          <div className={classes.divider} />
         </div>
-      </Paper>
-    </Grid>
+        <AttributeText
+          value={formatBool(file.metadata.hasEXIF, intl)}
+          icon={ExifIcon}
+          variant="primary"
+          size="small"
+        />
+        <div className={classes.dividerContainer}>
+          <div className={classes.divider} />
+        </div>
+        <VolumeOffOutlinedIcon className={classes.volume} />
+      </div>
+    </FpGridListItemContainer>
   );
 });
 
