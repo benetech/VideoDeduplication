d69438c14718c919fbff5c0299e74b4ab3937f10
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-09-19 02:02:50 +0700

    Add Database convenience class

---
+++ db/__init__.py
@@ -0,0 +1,49 @@
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from .schema import Base
+
+
+# TODO: Move to db.utils
+class Database:
+    """Database class provides session factory and convenience methods to access database."""
+
+    @staticmethod
+    def in_memory(**options):
+        """Create in-memory database."""
+        return Database('sqlite:///:memory:', **options)
+
+    def __init__(self, uri, base=Base, **options):
+        """Create a new database instance.
+
+        Each database instance will allocate it's own resources (connections, etc.).
+
+        Args:
+            uri (String): Database connection uri.
+        """
+        self.engine = create_engine(uri, **options)
+        self.session = sessionmaker(bind=self.engine)
+        self.base = base
+
+    def create_tables(self):
+        """Creates all tables specified on our internal schema."""
+        self.base.metadata.create_all(bind=self.engine)
+
+    def drop_tables(self):
+        """Drop database."""
+        self.base.metadata.drop_all(bind=self.engine)
+
+    @contextmanager
+    def session_scope(self):
+        """Provide a transactional scope."""
+        session = self.session()
+        try:
+            yield session
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
