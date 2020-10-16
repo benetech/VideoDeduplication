673bcec0a49126609e2a6896fa14ba13a5fa3c59
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-10-16 23:17:06 +0700

    Resolve linting issues

--- server/server/api/videos.py
+++ server/server/api/videos.py
@@ -5,13 +5,12 @@ from flask import abort, send_from_directory
 
 from db.schema import Files
 from .blueprint import api
-from .helpers import get_config, resolve_video_file_path
+from .helpers import resolve_video_file_path
 from ..model import database
 
 
 @api.route('/files/<int:file_id>/watch')
 def watch_video(file_id):
-    config = get_config()
     file = database.session.query(Files).filter(Files.id == file_id).first()
 
     # Handle file not found
