f4816297e766d475e9ee3d1b0f6c22009368b44f
Felipe Batista <fsbatista1@gmail.com>
2020-10-30 20:20:56 +0000

    additional refactor and linting fixes

--- winnow/feature_extraction/extraction_routine.py
+++ winnow/feature_extraction/extraction_routine.py
@@ -21,7 +21,13 @@ def pload_video(p, size, frame_sampling):
     return load_video(p, size, frame_sampling)
 
 
-def feature_extraction_videos(model, video_list, reprs, reprkey, cores=4, batch_sz=8, frame_sampling=1,
+def feature_extraction_videos(model,
+                              video_list,
+                              reprs,
+                              reprkey,
+                              cores=4,
+                              batch_sz=8,
+                              frame_sampling=1,
                               save_frames=False):
     """
     Function that extracts the intermediate CNN features
@@ -31,12 +37,18 @@ def feature_extraction_videos(model, video_list, reprs, reprkey, cores=4, batch_
         cores: CPU cores for the parallel video loading
         batch_sz: batch size fed to the CNN network
         video_list: list of video to extract features
-        reprs (winnow.storage.repr_storage.ReprStorage): storage of video features
-        reprkey: function to convert video file paths to representation storage key.
+        reprs (winnow.storage.repr_storage.ReprStorage): storage of
+        video features
+        reprkey: function to convert video file paths to representation
+        storage key.
         frame_sampling: Minimal distance (in sec.) between frames to be saved.
         save_frames: Save normalized video frames.
     """
-    video_list = {i: video.strip() for i, video in enumerate(open(video_list, encoding="utf-8").readlines())}
+    video_list = {
+                 i: video.strip()
+                 for i, video in
+                 enumerate(open(video_list, encoding="utf-8").readlines())}
+
     print('\nNumber of videos: ', len(video_list))
     print('Storage directory: ', reprs)
     print('CPU cores: ', cores)
@@ -44,20 +56,27 @@ def feature_extraction_videos(model, video_list, reprs, reprkey, cores=4, batch_
 
     print('\nFeature Extraction Process')
     print('==========================')
-        
+
     pool = Pool(cores)
     future_videos = dict()
 
-    progress_bar = tqdm(range(np.max(list(video_list.keys()))+1), mininterval=1.0, unit='video')
+    progress_bar = tqdm(
+                        range(np.max(list(video_list.keys()))+1),
+                        mininterval=1.0,
+                        unit='video')
     for video in progress_bar:
 
         try:
             video_file_path = video_list[video]
             progress_bar.set_postfix(video=os.path.basename(video_file_path))
             if os.path.exists(video_file_path):
-                
+
                 if video not in future_videos:
-                    video_tensor = pload_video(video_file_path, model.desired_size, frame_sampling)
+                    video_tensor = pload_video(
+                                        video_file_path,
+                                        model.desired_size,
+                                        frame_sampling)
+
                 else:
                     video_tensor = future_videos[video].get()
                     del future_videos[video]
@@ -70,8 +89,11 @@ def feature_extraction_videos(model, video_list, reprs, reprkey, cores=4, batch_
                     if next_video in video_list and \
                         next_video not in future_videos and \
                             os.path.exists(video_list[next_video]):
-                        future_videos[next_video] = pool.apply_async(pload_video,
-                            args=[video_list[next_video], model.desired_size,frame_sampling])
+                        future_videos[next_video] = pool.apply_async(
+                            pload_video,
+                            args=[
+                                video_list[next_video],
+                                model.desired_size, frame_sampling])
 
                 # extract features
                 features = model.extract(video_tensor, batch_sz)
@@ -86,9 +108,8 @@ def feature_extraction_videos(model, video_list, reprs, reprkey, cores=4, batch_
             logger.error(e)
 
 
-
 def load_featurizer(PRETRAINED_LOCAL_PATH):
-    
+
     model = CNN_tf('vgg', PRETRAINED_LOCAL_PATH)
-    
-    return  model
+
+    return model
--- winnow/search_engine/template_matching.py
+++ winnow/search_engine/template_matching.py
@@ -16,7 +16,7 @@ from winnow.storage.repr_storage import ReprStorage
 class SearchEngine:
     def __init__(self, templates_root, reprs: ReprStorage, model):
 
-        templates_glob = os.path.join(templates_root,'*')
+        templates_glob = os.path.join(templates_root, '*')
 
         self.templates_root = templates_glob
         self.model = model
@@ -24,92 +24,116 @@ class SearchEngine:
         self.reprs = reprs
         self.template_cache = self.load_available_templates()
         self.results_cache = {}
-        
-        
-    def find_available_templates(self):
-    
-        folders = glob(self.templates_root)
-        available = dict(zip([x.split('/')[-1] for x in folders],folders))
-        
-        return available
-    
-    def load_templates(self,files):
-        resized = np.array([load_image(x,224) for x in files])
-        features = self.model.extract(resized,batch_sz=10)
-        return features
-    
-    def load_available_templates(self):
-        
-        cache = dict()
-        
-        for k,v in self.available_queries.items():
-            
-            cache[k] = self.load_templates(glob(v + '/**'))
-            
-        return cache
-        
-    def create_annotation_report(self,threshold = 0.07,fp = 'template_test.csv',queries = None,frame_sampling = 1):
 
-        """Creates an annotation report suitable for annotation (using our own Annotator class)
-        
+    def find_available_templates(self):
+
+        folders = glob(self.templates_root)
+        available = dict(
+                        zip([x.split('/')[-1] for x in folders], folders))
+
+        return available
+
+    def load_templates(self, files):
+
+        resized = np.array([load_image(x, 224) for x in files])
+        features = self.model.extract(resized, batch_sz=10)
+        return features
+
+    def load_available_templates(self):
+
+        cache = dict()
+
+        for k, v in self.available_queries.items():
+
+            cache[k] = self.load_templates(glob(v + '/**'))
+
+        return cache
+
+    def create_annotation_report(
+                                 self,
+                                 threshold=0.07,
+                                 fp='template_test.csv',
+                                 queries=None,
+                                 frame_sampling=1):
+
+        """Creates an annotation report suitable for annotation
+        (using our own Annotator class)
+
         Returns:
-            [pandas.DataFrame] -- Dataframe in the same format as the output from the "generate_matches.py" script
+            [pandas.DataFrame] -- Dataframe in the same format as the output
+            from the "generate_matches.py" script
         """
 
         def create_template_summary(files):
-            resized = np.array([load_image(x,224) for x in files])
+            resized = np.array([load_image(x, 224) for x in files])
             return resized
 
         if queries is None:
             for q in self.available_queries:
-                self.find(q,threshold=threshold,plot=False)
+                self.find(q, threshold=threshold, plot=False)
         else:
             for q in queries:
-                self.find(q,threshold=threshold,plot=False)
+                self.find(q, threshold=threshold, plot=False)
 
         print(self.available_queries)
 
         if self.results_cache:
-            
-            records = pd.DataFrame.from_records(self.results_cache,index=None).reset_index()
-            
+
+            records = pd.DataFrame.from_records(
+                            self.results_cache,
+                            index=None).reset_index()
+
             # Massage template matching results
-            df = pd.melt(records,id_vars=['level_0','level_1'])
+            df = pd.melt(records, id_vars=['level_0', 'level_1'])
             msk = df['variable'] != 'Unnamed: 0'
-            df = df.loc[msk,:]
+            df = df.loc[msk, :]
             # Convert df into a more suitable format to be saved
-            df.rename(columns = {'level_0':'fn','level_1':'sha256','variable':'template_name'},inplace=True)
-            additional_info = df.loc[:,['value']].to_dict('records')
+            df.rename(
+                columns={
+                    'level_0': 'fn',
+                    'level_1': 'sha256',
+                    'variable': 'template_name'},
+                inplace=True)
+
+            additional_info = df.loc[:, ['value']].to_dict('records')
             additional_info = [x['value'] for x in additional_info]
             add_df = pd.DataFrame.from_records(x for x in additional_info)
             df['distance'] = add_df['distance']
             df['closest_match'] = add_df['closest_match']
-            # This will adjust the time sampling to the sampling rate (ideally this should be sourced from the DB and not from the config file)
-            df['closest_match_time'] = df['closest_match'].apply(lambda x: datetime.timedelta(seconds=x * frame_sampling))
-            df.drop(columns=['value'],inplace=True)
+            # This will adjust the time sampling to the sampling rate (ideally
+            # this should be sourced from the DB and not from the config file)
+            df['closest_match_time'] = df['closest_match'].apply(
+                                                lambda x: datetime.timedelta(
+                                                    seconds=x * frame_sampling)
+                                                    )
+            df.drop(columns=['value'], inplace=True)
 
             return df
-            
 
         elif not self.available_queries:
-            raise Exception('No templates were found. Please check if the templates are located at {}'.format(self.templates_root))
-            
-        else:
-            raise Exception('No matches were found at the current distance configuration ({})'.format(threshold))
+            raise Exception('No templates were found at {}'.format(
+                                        self.templates_root))
 
-    
-    def find(self,query,threshold=0.07,plot=True):
-        
+        else:
+            raise Exception('No matches were found at \
+                            the current distance configuration ({})'.format(
+                                threshold))
+
+    def find(self, query, threshold=0.07, plot=True):
+
         feats = self.template_cache[query]
-        print('Loaded query embeddings',feats.shape)
+        print('Loaded query embeddings',
+              feats.shape)
         self.results_cache[query] = defaultdict()
         for repr_key in self.reprs.frame_level.list():
             try:
                 sample = self.reprs.frame_level.read(repr_key)
                 video_frames = self.reprs.frames.read(repr_key)
-                
-                distances = np.mean(cdist(feats,sample,metric='cosine'),axis=0)
-                
+
+                distances = np.mean(
+                                    cdist(feats, sample, metric='cosine'),
+                                    axis=0)
+
                 self.results_cache[query][repr_key] = dict()
 
                 if len(distances) > 0:
@@ -118,28 +142,38 @@ class SearchEngine:
                 else:
                     frame_of_interest_index = 0
                     min_d = 1.0
-                
-                
+
                 self.results_cache[query][repr_key]["distance"] = min_d
-                self.results_cache[query][repr_key]["closest_match"] = frame_of_interest_index
-        
+                self.results_cache[query][repr_key]["closest_match"] = \
+                    frame_of_interest_index
+
                 if (min_d < threshold) and plot:
-                    
-                    frame_of_interest = np.hstack(video_frames[frame_of_interest_index:][:5])
-                    plt.figure(figsize=(20,10))
+
+                    frame_of_interest = np.hstack(
+                                            video_frames[
+                                                frame_of_interest_index:][:5])
+                    plt.figure(figsize=(20, 10))
                     plt.imshow(frame_of_interest)
                     plt.show()
 
             except Exception as e:
-                print('Error:',e,distances,repr_key,frame_of_interest_index)
+                print(
+                    'Error:',
+                    e,
+                    distances,
+                    repr_key,
+                    frame_of_interest_index)
                 pass
 
 
-def download_sample_templates(TEMPLATES_PATH,DOWNLOAD_URL="https://s3.amazonaws.com/winnowpretrainedmodels/templates.tar.gz"):
-    if os.path.exists(TEMPLATES_PATH):
-        print('Templates Found',glob(TEMPLATES_PATH + '/**'))
+def download_sample_templates(
+       TEMPLATES_PATH,
+       URL="https://s3.amazonaws.com/winnowpretrainedmodels/templates.tar.gz"):
 
-    else:    
+    if os.path.exists(TEMPLATES_PATH):
+        print('Templates Found', glob(TEMPLATES_PATH + '/**'))
+
+    else:
         try:
             os.makedirs(TEMPLATES_PATH)
         except Exception as e:
@@ -147,8 +181,8 @@ def download_sample_templates(TEMPLATES_PATH,DOWNLOAD_URL="https://s3.amazonaws.
             pass
         print('Downloading sample templates to:{}'.format(TEMPLATES_PATH))
         DST = TEMPLATES_PATH + '/templates.tar.gz'
-        download_file(DST,DOWNLOAD_URL)
+        download_file(DST, URL)
         # unzip files
-        shutil.unpack_archive(DST,format='gztar')
+        shutil.unpack_archive(DST, format='gztar')
         # Delete tar
         os.unlink(DST)
