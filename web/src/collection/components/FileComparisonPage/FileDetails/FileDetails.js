d9e97197574f92e7032fef27e9042bec29dd4590
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-10-31 00:20:55 +0700

    Add comparable file component

---
+++ web/src/collection/components/FileComparisonPage/FileDetails/FileDetails.js
@@ -0,0 +1,53 @@
+import React, { useCallback, useState } from "react";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/styles";
+import { FileType } from "../../FileBrowserPage/FileType";
+import VideoPlayerPane from "../../VideoDetailsPage/VideoPlayerPane";
+import { seekTo } from "../../VideoDetailsPage/seekTo";
+import FileDescriptionPane from "./FileDescriptionPane";
+import FileDetailsHeader from "../MotherFile/FileDetailsHeader";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // display: "block",
+  },
+  pane: {
+    margin: theme.spacing(2),
+  },
+}));
+
+function FileDetails(props) {
+  const { file, className } = props;
+  const classes = useStyles();
+  const [player, setPlayer] = useState(null);
+
+  const handleJump = useCallback(seekTo(player, file), [player, file]);
+
+  return (
+    <div className={clsx(classes.root, className)}>
+      <VideoPlayerPane
+        collapsible
+        file={file}
+        onPlayerReady={setPlayer}
+        className={classes.pane}
+      />
+      <FileDescriptionPane
+        collapsible
+        file={file}
+        onJump={handleJump}
+        className={classes.pane}
+      />
+    </div>
+  );
+}
+
+FileDetails.propTypes = {
+  /**
+   * Video file to be displayed
+   */
+  file: FileType.isRequired,
+  className: PropTypes.string,
+};
+
+export default FileDetails;
