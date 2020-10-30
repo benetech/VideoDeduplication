63b4ece29e5ab381f533ab92896892584c095614
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-10-31 00:22:16 +0700

    Implement match file list

---
+++ web/src/collection/components/FileComparisonPage/MatchFiles/FileMatchHeader.js
@@ -0,0 +1,50 @@
+import React from "react";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/styles";
+import FileSummary from "../../FileSummary/FileSummary";
+import { FileType } from "../../FileBrowserPage/FileType";
+import Distance from "../../../../common/components/Distance";
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    boxShadow: "0 12px 18px 0 rgba(0,0,0,0.08)",
+    padding: theme.spacing(2),
+  },
+  name: {
+    paddingBottom: theme.spacing(2),
+  },
+}));
+
+function FileMatchHeader(props) {
+  const { distance, file, className, ...other } = props;
+  const classes = useStyles();
+  return (
+    <div className={clsx(classes.header, className)} {...other}>
+      <FileSummary file={file} className={classes.name}>
+        <FileSummary.Name />
+        <Distance value={distance} dense />
+      </FileSummary>
+      <FileSummary file={file} divider>
+        <FileSummary.Fingerprint />
+        <FileSummary.Duration />
+        <FileSummary.CreationDate />
+        <FileSummary.HasExif />
+      </FileSummary>
+    </div>
+  );
+}
+
+FileMatchHeader.propTypes = {
+  /**
+   * Distance to the matched file.
+   */
+  distance: PropTypes.number.isRequired,
+  /**
+   * Video file to be summarized.
+   */
+  file: FileType.isRequired,
+  className: PropTypes.string,
+};
+
+export default FileMatchHeader;
---
+++ web/src/collection/components/FileComparisonPage/MatchFiles/MatchFiles.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/styles";
+import { useIntl } from "react-intl";
+import LoadingHeader from "../LoadingHeader";
+import FileDetails from "../FileDetails";
+import useDirectMatches from "../../../hooks/useDirectMatches";
+import FileMatchHeader from "./FileMatchHeader";
+import MatchSelector from "./MatchSelector";
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  header: {
+    height: theme.spacing(10),
+    padding: theme.spacing(2),
+    display: "flex",
+    alignItems: "center",
+  },
+  title: {
+    ...theme.mixins.title3,
+    fontWeight: "bold",
+    flexGrow: 1,
+  },
+  loading: {
+    margin: theme.spacing(2),
+  },
+  fileHeader: {
+    marginTop: 0,
+    margin: theme.spacing(2),
+  },
+}));
+
+/**
+ * Get i18n text.
+ */
+function useMessages() {
+  const intl = useIntl();
+  return {
+    title: intl.formatMessage({ id: "file.match" }),
+    loadError: intl.formatMessage({ id: "match.load.error" }),
+  };
+}
+
+function MatchFiles(props) {
+  const { motherFileId, className, ...other } = props;
+  const classes = useStyles();
+  const messages = useMessages();
+  const [selected, setSelected] = useState(0);
+
+  const {
+    file,
+    matches,
+    error: matchError,
+    loadMatches,
+    hasMore,
+    progress,
+  } = useDirectMatches(motherFileId);
+
+  let content;
+  if (hasMore) {
+    content = (
+      <LoadingHeader
+        onRetry={loadMatches}
+        errorMessage={messages.loadError}
+        error={matchError}
+        className={classes.loading}
+        progress={progress}
+      />
+    );
+  } else if (matches.length > 0) {
+    content = (
+      <div>
+        <FileMatchHeader
+          distance={matches[selected].distance}
+          file={file}
+          className={classes.fileHeader}
+        />
+        <FileDetails file={matches[selected].file} />
+      </div>
+    );
+  } else {
+    console.log("MATCHES", matches);
+    content = null;
+  }
+
+  return (
+    <div className={clsx(classes.root, className)} {...other}>
+      <div className={classes.header}>
+        <div className={classes.title}>{messages.title}</div>
+        {!hasMore && (
+          <MatchSelector
+            matches={matches}
+            selected={selected}
+            onChange={setSelected}
+          />
+        )}
+      </div>
+      {content}
+    </div>
+  );
+}
+
+MatchFiles.propTypes = {
+  /**
+   * Mother file id.
+   */
+  motherFileId: PropTypes.number.isRequired,
+  className: PropTypes.string,
+};
+
+export default MatchFiles;
