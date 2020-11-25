a257a7168255d1ad4dc0fd4f4a5ce14c436d27b6
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-11-25 19:16:29 +0700

    Cancel unfinished fetch on params update.

--- web/src/collection/state/fileCluster/sagas.js
+++ web/src/collection/state/fileCluster/sagas.js
@@ -1,5 +1,6 @@
 import {
   ACTION_FETCH_FILE_CLUSTER_SLICE,
+  ACTION_UPDATE_FILE_CLUSTER_PARAMS,
   fetchFileClusterSliceFailure,
   fetchFileClusterSliceSuccess,
 } from "./actions";
@@ -9,7 +10,11 @@ import fetchEntitiesSaga from "../fetchEntities/fetchEntitiesSaga";
 /**
  * Fetch the next slice of file cluster items.
  */
-function* fetchFileClusterSliceSaga(server, selectFileCluster) {
+function* fetchFileClusterSliceSaga(server, selectFileCluster, action) {
+  // Handling update-params is required to cancel the previous request.
+  if (action.type === ACTION_UPDATE_FILE_CLUSTER_PARAMS) {
+    return;
+  }
   yield* fetchEntitiesSaga({
     requestResource: [server, server.fetchFileCluster],
     stateSelector: selectFileCluster,
@@ -25,7 +30,7 @@ function* fetchFileClusterSliceSaga(server, selectFileCluster) {
 export default function* fileClusterRootSaga(server, selectFileCluster) {
   // Handle every slice fetch.
   yield takeLatest(
-    ACTION_FETCH_FILE_CLUSTER_SLICE,
+    [ACTION_FETCH_FILE_CLUSTER_SLICE, ACTION_UPDATE_FILE_CLUSTER_PARAMS],
     fetchFileClusterSliceSaga,
     server,
     selectFileCluster
