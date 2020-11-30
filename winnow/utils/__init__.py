c3e8d423478994dfa0f110f3d981bb23d4b65a09
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-11-30 22:43:48 +0700

    Clarify naming

--- winnow/utils/__init__.py
+++ winnow/utils/__init__.py
@@ -1,4 +1,4 @@
 # TODO: Optimize imports (#158)
-from .utils import *  # noqa: F401, F403
+from .brightness import *  # noqa: F401, F403
 from .scene_detection import *  # noqa: F401, F403
 from .metadata_extraction import *  # noqa: F401, F403
