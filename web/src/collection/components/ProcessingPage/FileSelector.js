a7e19fd11e1786332074d73c6882864e124ae4db
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-12-11 23:58:07 +0700

    Create trigger stub button

--- web/src/collection/components/ProcessingPage/FileSelector.js
+++ web/src/collection/components/ProcessingPage/FileSelector.js
@@ -2,18 +2,28 @@ import React from "react";
 import clsx from "clsx";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/styles";
+import Button from "../../../common/components/Button";
 
 const useStyles = makeStyles((theme) => ({
   selector: {
     height: 600,
     border: "4px dashed #D8D8D8",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
 }));
 
 function FileSelector(props) {
   const { className } = props;
   const classes = useStyles();
-  return <div className={clsx(classes.selector, className)}></div>;
+  return (
+    <div className={clsx(classes.selector, className)}>
+      <Button color="primary" variant="outlined">
+        Process Entire Dataset
+      </Button>
+    </div>
+  );
 }
 
 FileSelector.propTypes = {
