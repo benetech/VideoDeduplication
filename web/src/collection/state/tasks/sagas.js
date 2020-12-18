05a98cfde394d2e5b57a6f61e479147a5995903f
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-12-19 02:04:42 +0700

    Listen to task updates on frontend

--- web/src/collection/state/tasks/sagas.js
+++ web/src/collection/state/tasks/sagas.js
@@ -1,11 +1,46 @@
-import { takeLatest } from "redux-saga/effects";
+import { call, fork, put, take, takeLatest } from "redux-saga/effects";
+import { eventChannel } from "redux-saga";
 import {
   ACTION_FETCH_TASK_SLICE,
   ACTION_UPDATE_TASKS_PARAMS,
   fetchTaskSliceFailure,
   fetchTaskSliceSuccess,
+  updateTask,
 } from "./actions";
 import fetchEntitiesSaga from "../fetchEntities/fetchEntitiesSaga";
+import io from "socket.io-client";
+import { SocketEvent, SocketNamespace, socketPath } from "./socket";
+import Transform from "../../../server-api/Server/Transform";
+
+function makeTaskChannel() {
+  return eventChannel((emit) => {
+    const transformer = new Transform();
+    const socket = io(SocketNamespace.TASKS, {
+      path: socketPath,
+    });
+    socket.on("connect", () => console.log("Connected!"));
+    socket.on("disconnect", () => console.log("Disconnected!"));
+    socket.on(SocketEvent.TASK_UPDATED, (data) => {
+      emit(updateTask(transformer.task(data)));
+    });
+    return () => {
+      socket.close();
+    };
+  });
+}
+
+function* handleTaskUpdatesSaga() {
+  try {
+    const channel = yield call(makeTaskChannel);
+
+    while (true) {
+      const action = yield take(channel);
+      yield put(action);
+    }
+  } catch (error) {
+    console.error("Task-updates saga error", error);
+  }
+}
 
 /**
  * Fetch the next slice of background tasks collection.
@@ -28,6 +63,9 @@ function* fetchTaskSliceSaga(server, selectTasks, action) {
  * Initialize task-related sagas...
  */
 export default function* taskRootSaga(server, selectTasks) {
+  // Handle task updates
+  yield fork(handleTaskUpdatesSaga);
+
   // Handle every slice fetch.
   yield takeLatest(
     [ACTION_FETCH_TASK_SLICE, ACTION_UPDATE_TASKS_PARAMS],
