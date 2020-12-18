f0c5664067e0e7feb55ffdf5f81e28cb5e8b82f7
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-12-18 20:18:14 +0700

    Configure celery worker to send task events

--- task_queue/application.py
+++ task_queue/application.py
@@ -8,7 +8,7 @@ BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
 
 # Create application
 celery_application = Celery("winnow-pipeline", broker=BROKER, backend=BACKEND, include=["task_queue.tasks"])
-celery_application.conf.update(result_extended=True)
+celery_application.conf.update(result_extended=True, worker_send_task_events=True, task_send_sent_event=True)
 
 if __name__ == "__main__":
     celery_application.start()
