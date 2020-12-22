f8425d800f82d58fea0a26a9b6204d9decf31ea2
Felipe Batista <fsbatista1@gmail.com>
2020-12-22 19:21:27 -0300

    Added list of files as argument to Generate matches

--- generate_matches.py
+++ generate_matches.py
@@ -1,18 +1,30 @@
 import click
 
 from winnow.pipeline.generate_matches import generate_matches
+from winnow.utils.files import scan_videos_from_txt
 from winnow.utils.config import resolve_config
 from winnow.utils.logging import configure_logging_cli
 
 
 @click.command()
 @click.option("--config", "-cp", help="path to the project config file", default=None)
-def main(config):
+@click.option(
+    "--list-of-files",
+    "-lof",
+    help="path to txt with a list of files for processing - overrides source folder from the config file",
+    default=None,
+)
+def main(config, list_of_files):
     logger = configure_logging_cli()
     logger.info("Loading config file")
     config = resolve_config(config_path=config)
+    files = []
+    print(list_of_files)
+    if list_of_files:
+        files = scan_videos_from_txt(list_of_files, extensions=config.sources.extensions)
+        print(files, "after scan")
 
-    generate_matches(config)
+    generate_matches(config, files=files)
 
 
 if __name__ == "__main__":
