483650b4678d32cc3ed68d437a62e8e77b602434
Stepan Anokhin <stepan.anokhin@gmail.com>
2020-12-24 21:16:55 +0700

    Capture celery.task logger output

--- task_queue/celery_config.py
+++ task_queue/celery_config.py
@@ -11,4 +11,7 @@ def configure_logging(**_):
     log_directory = os.environ.get("CELERY_LOG_FOLDER", "./data/task_logs")
     handler = QueueLogHandler(directory=log_directory, level=logging.INFO)
     handler.setFormatter(logging.Formatter("[%(asctime)s: %(levelname)s] [%(name)s] %(message)s"))
+    task_logger = logging.getLogger("celery.task")
+    task_logger.addHandler(handler)
+    task_logger.propagate = True
     logging.root.addHandler(handler)
