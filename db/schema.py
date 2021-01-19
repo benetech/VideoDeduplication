2171965bf2f4d81fc128ddcef44115946cceca84
Stepan Anokhin <stepan.anokhin@gmail.com>
2021-01-19 22:56:43 +0700

    Update schema to support remote fingerprints

--- db/schema.py
+++ db/schema.py
@@ -1,4 +1,5 @@
 import datetime
+import enum
 
 from sqlalchemy import (
     Column,
@@ -12,6 +13,8 @@ from sqlalchemy import (
     UniqueConstraint,
     DateTime,
     event,
+    CheckConstraint,
+    Enum,
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, object_session
@@ -21,12 +24,28 @@ Base = declarative_base()
 
 class Files(Base):
     __tablename__ = "files"
-    __table_args__ = (UniqueConstraint("file_path", "sha256", name="_file_uc"),)
+    __table_args__ = (
+        UniqueConstraint("file_path", "sha256", name="_file_uc"),
+        # contributor_id and external_id are either both null (if the file is local)
+        # or both not null (if the file is pulled from some remote repository).
+        CheckConstraint(
+            "(contributor_id IS NULL AND external_id IS NULL) OR "
+            "(contributor_id IS NOT NULL AND external_id IS NOT NULL)"
+        ),
+    )
 
     id = Column(Integer, primary_key=True)
     created_date = Column(DateTime, default=datetime.datetime.utcnow)
     sha256 = Column(String)
     file_path = Column(String)
+
+    # Remote fingerprints only attributes
+
+    contributor_id = Column(Integer, ForeignKey("contributors.id"), nullable=True)
+    external_id = Column(Integer, nullable=True)
+
+    # Relationships
+
     signature = relationship("Signature", uselist=False, back_populates="file")
     meta = relationship("VideoMetadata", uselist=False, back_populates="file")
     scenes = relationship("Scene", back_populates="file")
@@ -41,6 +60,8 @@ class Files(Base):
     target_matches = relationship(
         "Matches", back_populates="match_video_file", foreign_keys="Matches.match_video_file_id"
     )
+
+    contributor = relationship("Contributor", back_populates="files")
 
 
 class Signature(Base):
@@ -113,7 +134,6 @@ class Matches(Base):
     distance = Column(Float, nullable=False)
 
 
-# TODO: Change to date columns to datetime type (#137)
 class Exif(Base):
     __tablename__ = "exif"
 
@@ -148,3 +168,52 @@ class Exif(Base):
     Audio_Encoded_Date = Column(DateTime)
     Audio_Tagged_Date = Column(DateTime)
     Json_full_exif = Column(JSON)
+
+
+class RepositoryType(enum.Enum):
+    """Repository type determines its access method.
+
+    There might be different repository types (bare database, REST API, etc.),
+    so we need a way to specify which supported access method should be used.
+    """
+
+    BARE_DATABASE = "BARE_DATABASE"
+
+
+class Repository(Base):
+    """Remote fingerprint repository."""
+
+    __tablename__ = "repositories"
+
+    id = Column(Integer, primary_key=True)
+    # Human-readable repository name which will be displayed by interface
+    name = Column(String, nullable=False)
+    # Repository type determines how to access the repository
+    repository_type = Column(Enum(RepositoryType), nullable=False, default=RepositoryType.BARE_DATABASE)
+    # Address format may depend on repository type
+    network_address = Column(String, nullable=False)
+    # Account id or username which we use to access the repository
+    account_id = Column(String, nullable=False)
+    # Credentials ID in a secured credentials store
+    credentials_id = Column(String, nullable=False)
+
+    # Relationships
+
+    contributors = relationship("Contributor", back_populates="repository")
+
+
+class Contributor(Base):
+    """An organization that contributed some fingerprints to the remote repository."""
+
+    __tablename__ = "contributors"
+
+    id = Column(Integer, primary_key=True)
+    # Any identifier used in the corresponding repository to designate this contributor
+    name = Column(String, nullable=False)
+    # A repository from which this contributor is known
+    repository_id = Column(Integer, ForeignKey("repositories.id"), nullable=False)
+
+    # Relationships
+
+    repository = relationship("Repository", back_populates="contributors")
+    files = relationship("Files", back_populates="contributor")
