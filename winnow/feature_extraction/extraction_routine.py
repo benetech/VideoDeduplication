bf43214cbcf07b29a53137a60f2df72e5bbc61f1
Stepan Anokhin <stepan.anokhin@gmail.com>
2021-02-10 20:05:35 +0700

    Decouple feature extraction from pipeline resources

--- winnow/feature_extraction/extraction_routine.py
+++ winnow/feature_extraction/extraction_routine.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from typing import Callable
 
 import numpy as np
 from tqdm import tqdm
@@ -7,7 +8,6 @@ from tqdm import tqdm
 from winnow.utils.multiproc import multiprocessing as mp
 from .model_tf import CNN_tf
 from .utils import load_video
-from ..pipeline.progress_monitor import ProgressMonitor
 
 logger = logging.getLogger(__name__)
 
@@ -21,35 +21,27 @@ def pload_video(p, size, frame_sampling):
 
 def feature_extraction_videos(
     model,
-    video_list,
-    reprs,
-    reprkey,
+    videos,
+    on_extracted: Callable,
     cores=4,
     batch_sz=8,
     frame_sampling=1,
-    save_frames=False,
-    progress_monitor=ProgressMonitor.NULL,
 ):
     """
     Function that extracts the intermediate CNN features
     of each video in a provided video list.
     Args:
         model: CNN network
+        videos: list of video file paths
+        on_extracted: a callback receiving (file_path, frames_tensor, frames_features) to handle
+            extracted file features which is invoked on each file processing finish
         cores: CPU cores for the parallel video loading
         batch_sz: batch size fed to the CNN network
-        video_list: list of video to extract features
-        reprs (winnow.storage.repr_storage.ReprStorage): storage of
-        video features
-        reprkey: function to convert video file paths to representation
-        storage key.
         frame_sampling: Minimal distance (in sec.) between frames to be saved.
-        save_frames: Save normalized video frames.
-        progress_monitor (ProgressMonitor): a progress monitor for feature extraction routine.
     """
-    video_list = {i: video.strip() for i, video in enumerate(open(video_list, encoding="utf-8").readlines())}
+    video_list = {i: video for i, video in enumerate(videos)}
 
     logger.info("Number of videos: %s", len(video_list))
-    logger.info("Storage directory: %s", reprs)
     logger.info("CPU cores: %s", cores)
     logger.info("Batch size: %s", batch_sz)
     logger.info("Starting Feature Extraction Process")
@@ -57,7 +49,6 @@ def feature_extraction_videos(
     pool = mp.Pool(cores)
     future_videos = dict()
 
-    progress_monitor.scale(total_work=len(video_list))
     progress_bar = tqdm(range(np.max(list(video_list.keys())) + 1), mininterval=1.0, unit="video")
     for video in progress_bar:
 
@@ -88,17 +79,9 @@ def feature_extraction_videos(
 
                 # extract features
                 features = model.extract(video_tensor, batch_sz)
-
-                # save features
-                key = reprkey(video_file_path)
-                reprs.frame_level.write(key, features)
-                if save_frames:
-                    reprs.frames.write(key, video_tensor)
+                on_extracted(video_file_path, video_tensor, features)
         except Exception:
             logger.exception(f"Error processing file:{video_list[video]}")
-        finally:
-            progress_monitor.increase(1)
-    progress_monitor.complete()
 
 
 def load_featurizer(pretrained_local_path):
--- winnow/feature_extraction/intermediate_cnn.py
+++ winnow/feature_extraction/intermediate_cnn.py
@@ -1,29 +1,23 @@
+from typing import Callable
+
 from .extraction_routine import feature_extraction_videos, load_featurizer
 from .model import default_model_path
-from ..pipeline.progress_monitor import ProgressMonitor
 
 
 class IntermediateCnnExtractor:
-    def __init__(self, video_src, reprs, reprkey, frame_sampling=1, save_frames=False, model=None):
-
-        self.video_src = video_src
-        self.reprs = reprs
-        self.reprkey = reprkey
+    def __init__(self, videos, on_extracted: Callable, frame_sampling=1, model=None):
+        self.videos = videos
         self.frame_sampling = frame_sampling
-        self.save_frames = save_frames
         self.model = model
+        self.on_extracted = on_extracted
 
-    def start(self, batch_size=8, cores=4, progress_monitor=ProgressMonitor.NULL):
-        print("Starting feature extraction process: {}".format(self.video_src))
+    def extract_features(self, batch_size=8, cores=4):
         self.model = self.model or load_featurizer(default_model_path())
         feature_extraction_videos(
             model=self.model,
-            video_list=self.video_src,
-            reprs=self.reprs,
-            reprkey=self.reprkey,
+            videos=self.videos,
+            on_extracted=self.on_extracted,
             batch_sz=batch_size,
             cores=cores,
             frame_sampling=self.frame_sampling,
-            save_frames=self.save_frames,
-            progress_monitor=progress_monitor,
         )
