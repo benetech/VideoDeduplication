e3ad99d8bc2836e8c10378f5aac06f4286ff7e23
Stepan Anokhin <stepan.anokhin@gmail.com>
2021-02-11 22:01:03 +0700

    Disable cleaning by default

--- cli/cli/handlers/migration.py
+++ cli/cli/handlers/migration.py
@@ -10,10 +10,10 @@ class MigrationCli:
     def __init__(self, config):
         self._config = config
 
-    def reprs(self, clean_legacy: bool = True):
+    def reprs(self, clean: bool = False):
         """Migrate representations storage from legacy path-storage to lmdb-storage."""
         configure_logging_cli()
-        clean_legacy = boolean("clean_legacy", clean_legacy)
+        clean = boolean("clean", clean)
         path_to_lmdb = PathToLMDBMigration(config=self._config)
-        path_to_lmdb.migrate_all_inplace(clean_source=clean_legacy)
+        path_to_lmdb.migrate_all_inplace(clean_source=clean)
         print(ok("OK"))
