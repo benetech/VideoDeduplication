1a0c8e410334578282d3719b224a1142a5cb2374
Stepan Anokhin <stepan.anokhin@gmail.com>
2021-03-17 20:06:34 +0700

    Augment template config

--- winnow/config/config.py
+++ winnow/config/config.py
@@ -115,10 +115,16 @@ class TemplatesConfig:
     """Configuration for template matching."""
 
     source_path: str = None
+    distance: float = 0.07
+    distance_min: float = 0.05
+    override: bool = False  # Override template matches
 
     def read_env(self):
         """Read config from environment variables."""
         self.source_path = os.environ.get("WINNOW_TEMPLATE_SOURCE_PATH", self.source_path)
+        self.distance = float(os.environ.get("WINNOW_TEMPLATE_DISTANCE", self.distance))
+        self.distance_min = float(os.environ.get("WINNOW_TEMPLATE_DISTANCE_MIN", self.distance_min))
+        self.override = float(os.environ.get("WINNOW_TEMPLATE_OVERRIDE", self.override))
 
 
 @dataclass
