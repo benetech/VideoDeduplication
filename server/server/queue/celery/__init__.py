d7a990d03afcf4f90bf0e9ce38bc26dae97c8855
Stepan Anokhin <stepan.anokhin@gmail.com>
2021-03-18 19:38:01 +0700

    Add template-matching task to task-queue

--- server/server/queue/celery/__init__.py
+++ server/server/queue/celery/__init__.py
@@ -1,4 +1,4 @@
-from server.queue.model import ProcessDirectory, ProcessFileList, TestTask
+from server.queue.model import ProcessDirectory, ProcessFileList, TestTask, MatchTemplates
 from server.queue.request_transformer import RequestTransformer
 
 
@@ -7,7 +7,7 @@ def make_celery_task_queue(task_request_transformer: RequestTransformer):
     from server.queue.celery.backend import resolve_backend
     from server.queue.celery.task_queue import CeleryTaskQueue
     from task_queue.application import celery_application
-    from task_queue.tasks import test_fibonacci, process_file_list, process_directory
+    from task_queue.tasks import test_fibonacci, process_file_list, process_directory, match_all_templates
 
     return CeleryTaskQueue(
         app=celery_application,
@@ -16,6 +16,7 @@ def make_celery_task_queue(task_request_transformer: RequestTransformer):
         requests={
             ProcessDirectory: process_directory,
             ProcessFileList: process_file_list,
+            MatchTemplates: match_all_templates,
             TestTask: test_fibonacci,
         },
     )
