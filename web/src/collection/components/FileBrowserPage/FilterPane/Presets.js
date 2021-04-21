5bbd80b86c2e827e7539eaf7440db10cabcae2a2
Stepan Anokhin <stepan.anokhin@gmail.com>
2021-04-21 16:40:58 +0700

    Hook up preset list

--- web/src/collection/components/FileBrowserPage/FilterPane/Presets.js
+++ web/src/collection/components/FileBrowserPage/FilterPane/Presets.js
@@ -1,69 +1,79 @@
-import React from "react";
+import React, { useCallback } from "react";
 import clsx from "clsx";
+import lodash from "lodash";
 import PropTypes from "prop-types";
-import { MatchCategory } from "../../../state/fileList/MatchCategory";
-import { FileSort } from "../../../state/fileList/FileSort";
-
+import initialState from "../../../state/fileList/initialState";
 import { makeStyles } from "@material-ui/styles";
 import PresetList from "./PresetList";
+import { useDispatch } from "react-redux";
+import { updateFilters } from "../../../state/fileList/actions";
+import LoadTrigger from "../../../../common/components/LoadingTrigger/LoadTrigger";
+import { useIntl } from "react-intl";
+import PresetAPI from "./PresetAPI";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     paddingTop: theme.spacing(2),
   },
+  trigger: {
+    height: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 }));
 
-const presets = [
-  {
-    id: 1,
-    name: "Foo Dossier DossierDossierDossierDossierDossierDossier",
-    filters: {
-      query: "/foo/",
-      extensions: [],
-      length: { lower: null, upper: null },
-      date: { lower: null, upper: null },
-      audio: null,
-      exif: null,
-      matches: MatchCategory.all,
-      sort: FileSort.date,
-      remote: null,
-      templates: [],
-    },
-  },
-  {
-    id: 2,
-    name: "Remote",
-    filters: {
-      query: "",
-      extensions: [],
-      length: { lower: null, upper: null },
-      date: { lower: null, upper: null },
-      audio: null,
-      exif: null,
-      matches: MatchCategory.all,
-      sort: FileSort.date,
-      remote: true,
-      templates: [],
-    },
-  },
-];
+/**
+ * Get translated text.
+ */
+function useMessages() {
+  const intl = useIntl();
+  return {
+    error: intl.formatMessage({ id: "presets.loadError" }),
+  };
+}
 
 function Presets(props) {
   const { className } = props;
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const messages = useMessages();
+  const presetApi = PresetAPI.use();
+  const presetList = presetApi.useLazyPresetList();
+  console.log(presetList);
+
+  const handleApply = useCallback((preset) => {
+    const filters = lodash.merge({}, initialState.filters, preset.filters);
+    dispatch(updateFilters(filters));
+  });
+
+  const handleDelete = useCallback(
+    async (preset) => {
+      await presetApi.deletePreset(preset);
+    },
+    [presetApi]
+  );
 
   return (
     <div className={clsx(classes.root, className)}>
       <PresetList>
-        {presets.map((preset, index) => (
+        {presetList.presets.map((preset, index) => (
           <PresetList.Item
             key={preset.id}
             preset={preset}
-            onClick={console.log}
-            onDelete={console.log}
+            onClick={handleApply}
+            onDelete={handleDelete}
             divider={index < presets.length - 1}
           />
         ))}
+        <LoadTrigger
+          loading={presetList.isLoading}
+          onLoad={presetList.loadMore}
+          hasMore={presetList.hasMore}
+          errorMessage={messages.error}
+          error={presetList.error}
+          className={classes.trigger}
+        />
       </PresetList>
     </div>
   );
