74e89724d36014030446155e0df0c13cee9044c3
Stepan Anokhin <stepan.anokhin@gmail.com>
2021-04-29 15:09:38 +0700

    Define template-file-exclusion entity

--- db/schema.py
+++ db/schema.py
@@ -50,6 +50,7 @@ class Files(Base):
     meta = relationship("VideoMetadata", cascade="all,delete", uselist=False, back_populates="file")
     scenes = relationship("Scene", cascade="all,delete", back_populates="file")
     template_matches = relationship("TemplateMatches", back_populates="file", cascade="all, delete-orphan")
+    template_exclusions = relationship("TemplateFileExclusion", back_populates="file", cascade="all, delete-orphan")
     exif = relationship("Exif", cascade="all,delete", uselist=False, back_populates="file")
 
     # TODO: find a way to merge these two relationships
@@ -116,6 +117,7 @@ class Template(Base):
 
     examples = relationship("TemplateExample", cascade="all,delete", back_populates="template")
     matches = relationship("TemplateMatches", cascade="all,delete", back_populates="template")
+    file_exclusions = relationship("TemplateFileExclusion", cascade="all,delete", back_populates="template")
 
 
 class TemplateExample(Base):
@@ -157,6 +159,17 @@ class TemplateMatches(Base):
 
     file = relationship("Files", back_populates="template_matches")
     template = relationship("Template", back_populates="matches")
+
+
+class TemplateFileExclusion(Base):
+    """File excluded from the Template scope."""
+
+    __tablename__ = "template_file_exclusion"
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    file_id = Column(Integer, ForeignKey("files.id"), nullable=True)
+    template_id = Column(Integer, ForeignKey("templates.id"), nullable=True)
+    file = relationship("Files", back_populates="template_exclusions")
+    template = relationship("Template", back_populates="file_exclusions")
 
 
 @event.listens_for(Files.template_matches, "remove")
