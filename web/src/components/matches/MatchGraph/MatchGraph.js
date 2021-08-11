144f370a230dccc2835fdd10a67d73fbc3b7ecd7
Stepan Anokhin <stepan.anokhin@gmail.com>
2021-08-11 18:47:26 +0700

    Remove unused import

--- web/src/components/matches/MatchGraph/MatchGraph.js
+++ web/src/components/matches/MatchGraph/MatchGraph.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import clsx from "clsx";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/styles";
