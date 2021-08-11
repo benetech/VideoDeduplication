88e4ead613eaff804a0eccbf3decc4bf020d0db9
Stepan Anokhin <stepan.anokhin@gmail.com>
2021-08-11 18:03:53 +0700

    Prevent state update on unmounted components

--- web/src/lib/hooks/useStaging.js
+++ web/src/lib/hooks/useStaging.js
@@ -12,7 +12,9 @@ export default function useStaging(initialValue, delay = 200) {
     if (handle != null) {
       clearTimeout(handle);
     }
-    setHandle(setTimeout(() => setValue(staging), delay));
+    const newHandle = setTimeout(() => setValue(staging), delay);
+    setHandle(newHandle);
+    return () => clearTimeout(newHandle);
   }, [staging]);
 
   return { value, staging, setStaging, setValue };
