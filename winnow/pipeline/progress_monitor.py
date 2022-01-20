d17e911481b5d6600b7ad5a8f27ff6b98e17a6e2
Stepan Anokhin <stepan.anokhin@gmail.com>
2022-01-20 20:41:51 +0700

    Improve progress monitor

--- winnow/pipeline/progress_monitor.py
+++ winnow/pipeline/progress_monitor.py
@@ -1,12 +1,15 @@
 import abc
+import collections
+import math
 import sys
+from numbers import Number
 
 from tqdm import tqdm
 
 
 class BaseProgressMonitor(abc.ABC):
     @abc.abstractmethod
-    def scale(self, total_work):
+    def scale(self, total_work, unit: str = ""):
         """Change total amount of work."""
 
     @abc.abstractmethod
@@ -22,7 +25,7 @@ class BaseProgressMonitor(abc.ABC):
         """Increment amount of work done by the specified value."""
 
     @abc.abstractmethod
-    def subtask(self, work_amount):
+    def subtask(self, work_amount) -> "BaseProgressMonitor":
         """Create a subtask corresponding to the specified amount of work."""
 
     @property
@@ -35,12 +38,21 @@ class BaseProgressMonitor(abc.ABC):
     def total(self):
         """Get total work to be done."""
 
+    @abc.abstractmethod
+    def observe(self, observer):
+        """Add observer."""
+
+    def remaining(self) -> "BaseProgressMonitor":
+        """Get remaining work as a subtask."""
+        remaining_work = self.total - self.progress * self.total
+        return self.subtask(work_amount=remaining_work)
+
 
 class _NullProgressMonitor(BaseProgressMonitor):
     """A NoOp implementation of progress monitor."""
 
-    def scale(self, total_work):
-        pass
+    def scale(self, total_work, unit: str = ""):
+        return self
 
     def update(self, work_done):
         pass
@@ -62,25 +74,34 @@ class _NullProgressMonitor(BaseProgressMonitor):
     def total(self):
         return 0.0
 
-
-def _null_observer(progress, increase):
-    """No-op observer."""
+    def observe(self, observer):
+        pass
 
 
 class ProgressMonitor(BaseProgressMonitor):
     NULL = _NullProgressMonitor()
 
-    def __init__(self, observer=_null_observer, scale=1.0):
-        self._observer = observer
+    def __init__(self, observer=(), scale=1.0):
+        self._observers = self._init_observers(observer)
         self._total_work = float(scale)
         self._work_done = 0.0
 
-    def scale(self, total_work):
+    def _init_observers(self, observer):
+        """Initialize observers."""
+        if isinstance(observer, collections.Collection):
+            return list(observer)
+        elif observer is None:
+            return []
+        else:
+            return [observer]
+
+    def scale(self, total_work, unit: str = ""):
         """Change total amount of work."""
         if total_work < 0:
             raise ValueError(f"Negative total value: {total_work}")
         self._work_done = self.progress * total_work
         self._total_work = float(total_work)
+        return self
 
     def update(self, work_done):
         """Update current amount of work done."""
@@ -92,7 +113,8 @@ class ProgressMonitor(BaseProgressMonitor):
         else:
             change = 0.0
         self._work_done = work_done
-        self._observer(self.progress, change)
+        for observer in self._observers:
+            observer(self.progress, change)
 
     def complete(self):
         """Mark the task as completed."""
@@ -123,6 +145,9 @@ class ProgressMonitor(BaseProgressMonitor):
         """Get total work scale."""
         return self._total_work
 
+    def observe(self, observer):
+        self._observers.append(observer)
+
 
 class ProgressBar(BaseProgressMonitor):
     """Wrapper around a progress-monitor which will display a progress bar on the console."""
@@ -132,9 +157,11 @@ class ProgressBar(BaseProgressMonitor):
         self._progress_bar: tqdm = tqdm(file=file, total=self._monitor.total, unit=unit, **kwargs)
         self._progress_bar.update(self._monitor.progress * self._monitor.total)
 
-    def scale(self, total_work):
-        self._monitor.scale(total_work)
+    def scale(self, total_work, unit: str = "it"):
+        self._monitor.scale(total_work, unit)
         self._progress_bar.reset(total=total_work)
+        self._progress_bar.unit = unit
+        return self
 
     def update(self, work_done):
         current = self._monitor.total * self._monitor.progress
@@ -150,7 +177,17 @@ class ProgressBar(BaseProgressMonitor):
         self._progress_bar.update(amount)
 
     def subtask(self, work_amount):
-        return self._monitor.subtask(work_amount)
+        """Create progress bar subtask."""
+
+        # Make sure subtask updates also affect current progress bar
+        def subtask_observer(_, progress_change):
+            """Update the parent progress monitor."""
+            self.increase(progress_change * work_amount)
+
+        subtask: BaseProgressMonitor = self._monitor.subtask(work_amount)
+        subtask.observe(subtask_observer)
+
+        return subtask
 
     @property
     def progress(self):
@@ -159,3 +196,63 @@ class ProgressBar(BaseProgressMonitor):
     @property
     def total(self):
         return self._monitor.total
+
+    def observe(self, observer):
+        self._monitor.observe(observer)
+
+
+class LazyProgress(BaseProgressMonitor):
+    """
+    LazyProgress monitor updates notifies progress monitor only
+    when significant amount of work is done.
+    """
+
+    @staticmethod
+    def _suggest_portion(total_work: Number) -> Number:
+        """Suggest a "reasonable" work portion."""
+        threshold = 10000
+        if total_work <= threshold:
+            return float(total_work) / threshold
+        return int(total_work ** 0.5)
+
+    def __init__(self, delegate: ProgressMonitor, portion: Number = None):
+        self._accumulator = 0
+        self._delegate = delegate
+        self._portion_size = portion or self._suggest_portion(delegate.total)
+
+    def scale(self, total_work, unit: str = "it"):
+        factor = float(total_work) / self._delegate.total
+        self._accumulator *= factor
+        self._portion_size *= factor
+        self._delegate.scale(total_work, unit)
+        return self
+
+    def update(self, work_done):
+        self._delegate.update(work_done)
+        safe_work_done = self._delegate.progress * self._delegate.total
+        self._accumulator = safe_work_done - math.floor(safe_work_done / self._portion_size) * self._portion_size
+
+    def complete(self):
+        self._accumulator = 0
+        self._delegate.complete()
+
+    def increase(self, amount):
+        self._accumulator += amount
+        if self._accumulator > self._portion_size:
+            self._delegate.increase(self._accumulator)
+            self._accumulator = 0
+
+    def subtask(self, work_amount):
+        # Preserve lazyness
+        return LazyProgress(delegate=self._delegate.subtask(work_amount), portion=self._portion_size)
+
+    @property
+    def progress(self):
+        return self._delegate.progress
+
+    @property
+    def total(self):
+        return self._delegate.progress
+
+    def observe(self, observer):
+        self._delegate.observe(observer)
