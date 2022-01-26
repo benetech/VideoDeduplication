41fc081dc641910a207a89fb3f98d1983fbfa82b
Stepan Anokhin <stepan.anokhin@gmail.com>
2022-01-26 22:11:04 +0700

    Remove unused code

--- winnow/pipeline/luigi/utils.py
+++ winnow/pipeline/luigi/utils.py
@@ -173,115 +173,12 @@ class MatchesDF:
         return result
 
 
-def normalize_fingerprint_storage(config_path: str):
-    """Normalize fingerprint storage format."""
-    log_config_path = "./logging.conf"
-    if os.path.isfile(log_config_path):
-        logging.config.fileConfig(log_config_path)
-
-    pipeline = create_pipeline(config_path=config_path)
-    logger = logging.getLogger("winnow")
-
-    signature_store = pipeline.repr_storage.signature
-    storage_folder = signature_store.directory
-    logger.info("Normalizing signature storage in %s", storage_folder)
-
-    search_suffix = ".npy"
-    expected_suffix = "_vgg_features.npy"
-    search_pattern = os.path.join(storage_folder, f"**/*{search_suffix}")
-    sig_files = list(filter(os.path.isfile, glob(search_pattern, recursive=True)))
-    logger.info("Found %s candidate files", len(sig_files))
-
-    changed = 0
-    progress = ProgressBar(unit=" files")
-    progress.scale(len(sig_files))
-    portion, portion_size = 0, int(len(sig_files) ** 0.5)
-    for file_path in sig_files:
-        if not file_path.endswith(expected_suffix):
-            new_path = file_path[: -len(search_suffix)] + expected_suffix
-            shutil.move(file_path, new_path)
-            changed += 1
-        portion += 1
-        if portion >= portion_size:
-            progress.increase(portion)
-            portion = 0
-    progress.complete()
-    logger.info("Normalized %s files", changed)
-
-
-def prepare_ccweb(config_path: str):
-    """Prepare ccweb fingerprints."""
-    log_config_path = "./logging.conf"
-    if os.path.isfile(log_config_path):
-        logging.config.fileConfig(log_config_path)
-
-    pipeline = create_pipeline(config_path=config_path)
-    logger = logging.getLogger("winnow")
-
-    file_labels_csv = os.path.join(pipeline.config.repr.directory, "vcdb_files_labels.csv")
-    logger.info("Loading labeled ccweb videos list %s", file_labels_csv)
-    file_labels = pd.read_csv(file_labels_csv)
-    logger.info("Loaded %s labeled file names", len(file_labels.index))
-
-    logger.info("Creating file-keys dataframe")
-    file_keys = []
-    for entry in file_labels.itertuples():
-        file_keys.append((os.path.join(entry.label, entry.basename), ""))
-    file_keys_dataframe = pd.DataFrame(file_keys, columns=("path", "hash"))
-    logger.info("Created file-keys dataframe")
-
-    condensed_file_keys_path = os.path.join(pipeline.config.repr.directory, "condensed_fingerprints.files.csv")
-    logger.info("Saving condensed file-keys to %s", condensed_file_keys_path)
-    file_keys_dataframe.to_csv(condensed_file_keys_path)
-    logger.info("Saving file-keys is done")
-
-
-def read_fingerprints(
-    fingerprins_file: BinaryIO,
-    file_keys_file: TextIO,
-    logger: logging.Logger,
-) -> List[FeatureVector]:
-    """Read condensed fingerprints."""
-
-    fingerprints = np.load(fingerprins_file, allow_pickle=False)
-    file_keys = pd.read_csv(file_keys_file)
-    if len(fingerprints) != len(file_keys.index):
-        raise Exception(
-            "Inconsistent condensed vectors size: len(vectors) = %s != len(files) = %s",
-            len(fingerprints),
-            len(file_keys.index),
-        )
-
-    logger.info("Preparing fingerprints")
-
-    progress = ProgressBar(unit=" fingerprints")
-    progress.scale(len(file_keys.index))
-    portion, portion_size = 0, int(len(file_keys.index) ** 0.5)
-    result = []
-    for fingerprint, row in zip(fingerprints, file_keys.itertuples()):
-        result.append(FeatureVector(key=FileKey(path=row.path, hash=row.hash), features=fingerprint))
-        portion += 1
-        if portion >= portion_size:
-            progress.increase(portion)
-            portion = 0
-    progress.complete()
-    return result
-
-
 def random_mask(total_size, true_count) -> np.ndarray:
     """Create random True/False mask."""
     mask = np.full(total_size, fill_value=False)
     mask[: min(true_count, len(mask))] = True
     np.random.shuffle(mask)
     return mask
-
-
-def select_random_vectors(vectors: np.ndarray, max_count: int) -> np.ndarray:
-    """Select subset of vectors."""
-    if max_count <= 0:
-        return vectors
-    selected = random_mask(len(vectors), max_count)
-    return vectors[selected]
 
 
 def without_ext(path: PathLike) -> str:
